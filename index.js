265a88cc7301451926194447be8d9643811b2939
Samuel-Anuoluwagit config --global user.name Samuel-Anuoluwa <adewalesamuel653@gmail.com>
2023-09-04 09:31:03 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function alisym(x, y) {
+    let sum = sum - 10;
+    let sub = sub * 3;
+    let mult = mult / 2;
+    let div = mult % 3;
+    return `${sum} ${sub} ${mult} ${div}`;
+}
+
+console.log(3, 400);
